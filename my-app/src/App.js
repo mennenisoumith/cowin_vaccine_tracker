64f5e6055fd27ed581824be09bae5e270d181b4a
mennenisoumith <76424258+mennenisoumith@users.noreply.github.com>
2021-05-27 15:27:35 +0530

    made deploye,nt changes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,70 +7,16 @@ import CustomTable from './CustomTable';
 
 function App() {
   const [dataStorage, setDataStorage] = useState([]);
+  // const [session_id_storage, setsession_id_storage] = useState([]);
   // let data_storage = [];
   let session_id_storage = [];
   useEffect(() => {
-      timerCallingFunction();
+    console.log("in useeffect");
+     // eslint-disable-next-line no-unused-expressions
+  timerCallingFunction();
+    
   },[]);
-  // useEffect(() => {
-   
-  // },[data_storage]);
-  
-  // const extractData = (responseData) => {
-  //   if(responseData){
-  //     console.log('in useeffect of responseData dependency')
-  //     const arrayData=responseData;
-  //     var i;
-  //       for(i=0;i<arrayData.length;i++){
-  //         var individual_centre = arrayData[i];
-  //         if(individual_centre.min_age_limit <= 45 && individual_centre.vaccine === "COVISHIELD"){
-  //           if(individual_centre.available_capacity_dose1 >= 0 ){
-  //             console.log('individual_centre',individual_centre);
-  //             pastCentreDetails=presentCentreDetails;
-  //             presentCentreDetails=individual_centre;
-  //             console.log("inside if statemnt")
-  //             // setTimeout(timerCallingFunction, 5000);
-  //             if(presentCentreDetails.name!==pastCentreDetails.name)
-  //             console.log('inside sendmail if')
-  //             // sendMail();
-              
-  //           }
-  //         }
-  //       }
-  //   }
-  // }
 
-   
-
-  
-  // const extractNizamData = (responseData) => {
-  //   if(responseData){
-  //     const arrayData=responseData;
-  //     var i;
-  //     console.log("in if of extract nzb data")
-  //       for(i=0;i<arrayData.length;i++){
-  //         var individual_centre = arrayData[i];
-  //         var individualSession=individual_centre.sessions;
-  //         var j;
-  //         for(j=0;j<individualSession.length;j++){
-  //           if(individualSession[j].min_age_limit === 45 && individualSession[j].vaccine === "COVISHIELD"){
-  //             if(individualSession[j].available_capacity_dose1 >= 0 ){
-  //               console.log('individual_centre',individual_centre);
-  //               pastCentreDetails=presentCentreDetails;
-  //               presentCentreDetails=individual_centre;
-  //               console.log("inside if statemnt")
-  //               // setTimeout(timerCallingFunction, 5000);
-  //               if(presentCentreDetails.name!==pastCentreDetails.name)
-  //               console.log('inside sendmail if')
-  //               // sendMail();
-                
-  //             }
-  //           }
-  //         }
-  //       }
-  //   }
-  // }
- 
 
   const extractMedchalData = (responseData) => {
     if(responseData){
@@ -83,12 +29,9 @@ function App() {
           for(j=0;j<individual_centre.sessions.length;j++){
             var pre_session =individual_centre.sessions[j];
             // console.log("pre_session",pre_session)
-            if(pre_session.min_age_limit === 45 && pre_session.vaccine === "COVAXIN"){
-              if(pre_session.available_capacity_dose2 > 0 && !session_id_storage.includes(pre_session.session_id) ){
-                // console.log('individual centre obj struc',individual_centre);
-                // console.log('session obj struc',pre_session);
-                // console.log('new_session_id',pre_session.session_id)
-                // console.log('dataStorage before pushing',dataStorage);
+            if(pre_session.min_age_limit === 45 && pre_session.vaccine === "COVISHIELD"){
+              if(pre_session.available_capacity_dose2 >0 && !session_id_storage.includes(pre_session.session_id) ){
+                console.log('individual centre obj struc',individual_centre);
                 // eslint-disable-next-line no-loop-func
                 setDataStorage(dataStorage =>{
                   return [...dataStorage,{
@@ -96,10 +39,9 @@ function App() {
                     session_array_index:j,
                   }]
                 });
-
-                console.log('dataStorage after pushing',dataStorage);
                 session_id_storage.push(pre_session.session_id);
-                 // sendMail(individual_centre);
+                 sendMail(individual_centre);
+                 alert(`found centre ${individual_centre.name}`)
               }
             }
           }
@@ -125,7 +67,7 @@ function App() {
           // }
           let templateParams3 = {
             from_name: "saisoumith1818@gmail.com",
-            to_name:"krishnareddy.erla@gmail.com",
+            to_name:"bbhavana16@gmail.com",
             subject: "COVID VACCINATION",
             message: `Hi vaccination opened for centre with name  ${individual_centre.name}`,
           }
@@ -147,12 +89,13 @@ function App() {
   }
 
 
-  const timerCallingFunction = async () => {
+  const timerCallingFunction =  () => {
     // const apiUrl ="https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id=294&date=24-05-2021"
-    // const apiNizamabad="https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=600&date=25-05-2021"
-    const apiMedchal ="https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=596&date=25-05-2021";
+    // const apiNizamabad="https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=600&date=26-05-2021";
+    // const apiMedchal ="https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=596&date=26-05-2021";
+    const apiHyderabad = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=581&date=27-05-2021"
     axios
-    .get(apiMedchal)
+    .get(apiHyderabad)
     .then((response) => {
       // setResponseData(response.data.sessions);
       // extractData(response.data.sessions);
@@ -165,7 +108,7 @@ function App() {
 
     });
     console.log('calling api again')
-   await setTimeout(timerCallingFunction, 10000);
+   setTimeout(timerCallingFunction, 10000);
   }
 
   return (
